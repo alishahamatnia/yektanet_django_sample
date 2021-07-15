390eb3f2b5195b47e8c01fe24603d6ec15eac119
Ali Shahamatnia <ali.shahamatnia@gmail.com>
2021-07-15 07:15:10 +0430

    is_approved ad model

--- advertising_management/models.py
+++ advertising_management/models.py
@@ -14,6 +14,7 @@ class Ad(BaseAdvertising):
     title = models.CharField(max_length=30)
     link = models.URLField()
     img_url = models.URLField()
+    is_approved = models.BooleanField(default=False)
 
 
 class Click(models.Model):
